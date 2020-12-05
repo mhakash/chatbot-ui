162e309d339a3c0e11a217dbbe87738fcccd0f03
mhakash <mhakash211998@gmail.com>
2020-12-05 15:14:19 +0600

    initial commit

---
+++ components/chat.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useRef, useEffect } from "react";
+import Styles from "./styles.module.css";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import Image from "next/image";
+
+const Chat = () => {
+  const boxRef = useRef();
+  const [open, setOpen] = useState(false);
+  const [currentInput, setCurrentInput] = useState("");
+  const [received, setReceived] = useState([""]);
+  const [messages, setMessages] = useState([{ text: "hello there", type: 1, id: uuidv4() }]);
+  const [rtt, setRtt] = React.useState(0);
+
+  useEffect(() => {
+    setRtt(open ? 1 : 0);
+  }, [open]);
+
+  useEffect(() => {
+    const l = received.length;
+    if (received[l - 1]) {
+      setMessages([...messages, { text: received[l - 1], type: 1, id: uuidv4() }]);
+    }
+  }, [received.length]);
+
+  useEffect(() => {
+    if (open) {
+      const objDiv = boxRef.current;
+      objDiv.scrollTop = objDiv.scrollHeight;
+    }
+  }, [messages.length]);
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    if (currentInput) {
+      setMessages([...messages, { text: currentInput, type: 0, id: uuidv4() }]);
+
+      axios
+        .post("/api/hello", {
+          text: currentInput,
+        })
+        .then(function (response) {
+          setReceived([...received, response.data.text]);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+      setCurrentInput("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setCurrentInput(e.target.value);
+  };
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className={Styles.container}>
+      {open ? (
+        <div className={Styles.chatBox}>
+          <div className={Styles.chatTitle}>
+            SENSEBOT <div className={Styles.sub}>Try out now</div>
+          </div>
+          <div className={Styles.chatMessage} ref={boxRef}>
+            {messages.map((msg) => {
+              return (
+                <div className={msg.type === 0 ? Styles.sentMessage : Styles.receivedMessage} key={msg.id}>
+                  {msg.text}
+                </div>
+              );
+            })}
+          </div>
+          <div className={Styles.chatInputContainer}>
+            <form action="" onSubmit={handleInput} autoComplete="off">
+              <input
+                type="text"
+                name="chatInput"
+                value={currentInput}
+                placeholder="Type message here..."
+                className={Styles.chatInput}
+                onChange={handleChange}
+                // onKeyDown={handleInput}
+              />
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div className={Styles.messageNow}>
+          <div className={Styles.chatNowTitle}>
+            <Image
+              src="/logoOnly.jpg"
+              width={108}
+              height={130}
+              alt="sensebot-logo"
+              className={open ? Styles.rtt : Styles.normal}
+            />
+          </div>
+          <div className={Styles.chatNow} onClick={handleToggle}>
+            Chat Now
+          </div>
+        </div>
+      )}
+      <div className={Styles.toggleButton}>
+        <Image src="/chat.png" width={50} height={50} alt="chat-icon" onClick={handleToggle} rtt={rtt} />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export default async (req, res) => {
+  // console.log(req.body);
+  res.statusCode = 200;
+  const r = Math.floor(Math.random() * 16);
+  const msg = [
+    "hmm",
+    "I need more training",
+    "me so noob",
+    "sad day for you",
+    "why are you here",
+    "I am a joke",
+    "who are you btw",
+    "stop plz",
+    "i am too weak",
+    "forgive me",
+    "pardon plz",
+    "i am not prepared",
+    "i dont care what you ask",
+    "lol do you think I am smart?",
+    "too bad to answer",
+    "sing a song",
+  ];
+  await sleep(500);
+  res.json({ text: msg[r] });
+};
