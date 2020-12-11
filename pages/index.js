f9a644f82f67261b87844f04fb71ffd60eade663
mhakash <mhakash211998@gmail.com>
2020-12-12 00:01:01 +0600

    added video

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Chat from "../components/chat";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import { FaFacebook, FaLinkedin, FaGlobe } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -10,21 +11,47 @@ export default function Home() {
         <title>Sense-bot</title>
         <link rel="icon" href="/logoOnly.jpg" />
       </Head>
-
+      <div className={styles.top}>Powered by Sense Robotics & Intelsense AI</div>
       <main className={styles.main}>
-        <Image src="/logo.jpg" width={300} height={100} alt="sensebot-logo" />
-        {/* <h1 className={styles.title}>Sense-Bot !</h1> */}
+        <div className={styles.video}>
+          <video muted width="100%" autoPlay muted loop playsInline aria-hidden ata-object-fit>
+            <source src="/leadspace.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className={styles.mainContent}>
+          <div className={styles.logo}>
+            <Image src="/logo.png" width={387} height={120} alt="sensebot-logo" />
+          </div>
+          {/* <h1 className={styles.title}>Sense-Bot !</h1> */}
 
-        <p className={styles.description}>
-          The SenseBots platform provides the infrastructure and core capabilities to power an omni-channel
-          conversational exprience. Try out our work on the coversational AI technology now.
-        </p>
+          <p className={styles.description}>
+            The SenseBots platform provides the infrastructure and core capabilities to power an omni-channel
+            conversational exprience. Try out our work on the coversational AI technology now.
+          </p>
+        </div>
       </main>
       <Chat />
 
       <footer className={styles.footer}>
-        <div>
-          Powered by <a href="https://intelsense.ai/">Intelsense.ai</a>
+        <div className={styles.footertop}>
+          Made with love by <a href="https://intelsense.ai/">Intelsense.ai</a>
+        </div>
+        <div className={styles.links}>
+          <div className={styles.link}>
+            <a href="https://www.facebook.com/intelsenseai">
+              <FaFacebook />
+            </a>
+          </div>
+          <div className={styles.link}>
+            <a href="https://www.linkedin.com/company/intelsenseai/">
+              <FaLinkedin />
+            </a>
+          </div>
+          <div className={styles.link}>
+            <a href="https://intelsense.ai/">
+              <FaGlobe />
+            </a>
+          </div>
         </div>
       </footer>
     </div>
